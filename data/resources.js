edc3667c811377bbb3561a3de9b4e5c2816928c8
John Martin <jrmartin182@gmail.com>
2020-05-05 16:18:13 -0600

    added validation to POST

--- data/resources.js
+++ data/resources.js
@@ -6,6 +6,28 @@ const dbName = "learning_center";
 const collName = "resources";
 const settings = {useUnifiedTopology: true};
 
+const invalidArticle = (resource) => {
+    let result;
+    if(!resource.title){
+        result=("Article requires a title");
+    } else if(!resource.url){
+        result=("Article requires a url.");
+    } else if(!validURL(resource.url)){
+        result=("Not a valid URL.");
+    };
+    return result;
+};
+
+const validURL = (str) => {
+    const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return pattern.test(str);
+}
+
 const getResources = () => {
     const promise = new Promise((resolve, reject) => {
         MongoClient.connect(DB_URL, settings, async function(err, client){
@@ -29,16 +51,22 @@ const getResources = () => {
     return promise;
 };
 
-const addResource = (resource) => {
+const addResource =(resource) => {
     const promise = new Promise((resolve, reject) => {
+        const check = invalidArticle(resource);
+        if(check){
+            resource.invalid = check;
+        }
+         resource.invalid;
         MongoClient.connect(DB_URL, settings, async function(err, client){
             if(err){
-                reject(err)
+                reject(err);
             } else {
-                console.log("Successfully connect to DB for POST");
+                console.log("Successfully connecr to DB for POST.");
                 const db = client.db(dbName);
                 const collection = db.collection(collName);
-                await collection.insertOne(resource, (err, result) => {
+                resource.date_added = new Date(Date.now()).toUTCString();
+                collection.insertOne(resource, (err, result) =>{
                     if(err){
                         console.log(err);
                     } else {
@@ -48,6 +76,7 @@ const addResource = (resource) => {
                 })
             }
         })
+        
     });
     return promise;
 };
